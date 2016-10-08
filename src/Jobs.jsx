1a24cbd1134c0ed6027da6376dfb96ee47a8c331
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-10-08 15:04:59 -0400

    Added semantic-ui

--- src/Jobs.jsx
+++ src/Jobs.jsx
@@ -7,16 +7,30 @@ const propTypes = {
 
 function Jobs({ children }) {
   return (
-    <div>
+    <div className="ui two cards">
+      <div className="card">
+      <div className="content">
+       <img className="right floated mini ui image" src="/images/avatar/large/elliot.jpg"/>
+      <div className="header">
+        Elliot Fu
+      </div>
+      <div className="meta">
+        Friends of Veronika
+      </div>
       <p>
         This is an example page. Refresh the page or copy/paste the url to
         test out the redirect functionality (this same page should load
         after the redirect).
+
+
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto quaerat sed doloribus fugit commodi non debitis magnam fugiat ea vitae, facere sit veritatis saepe possimus, assumenda, ullam cumque adipisci blanditiis!
       </p>
       {children ||
         <div><Link to="/example/two-deep?field1=foo&field2=bar#boom!">
           Example two deep with query and hash
         </Link></div>}
+        </div>
+      </div>
     </div>
   );
 }
